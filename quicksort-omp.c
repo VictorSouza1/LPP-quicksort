752b512437ab12b9cdd7bb770e3bd85ddd42ca9a
victorsouza-mnz <victorsouzamnz@gmail.com>
2022-07-04 09:07:49 -0300

    add comments to quicksort omp

--- quicksort-omp.c
+++ quicksort-omp.c
@@ -2,7 +2,10 @@
 #include <time.h>
 #include <stdlib.h>
 #include "omp.h"
+#define ARR_SIZE 65536
+//#define ARR_SIZE 131072
 
+// Funcao auxiliar para fazer o swap
 void swap(int *arr, int i, int j)
 {
     int temp = arr[i];
@@ -10,7 +13,7 @@ void swap(int *arr, int i, int j)
     arr[j] = temp;
 }
 
-
+// Funcao que de fato preisa ser paralelizada
 int partition(int *arr, int left, int right)
 {
     int pivot = arr[right];
@@ -32,6 +35,9 @@ void quicksort(int *arr, int left, int right)
     if (left < right)
     {
         int partitionIdx = partition(arr, left, right);
+
+        //Utilizamos as tasks o OMP para paralelizar as chamadas recursivas
+        // Firstprivate : Especifica que cada thread deve ter sua própria instância de uma variável e que a variável deve ser inicializada com o valor da variável, pois ela existe antes da construção paralela.
         #pragma omp task firstprivate(arr,left,partitionIdx)
         {
             quicksort(arr, left, partitionIdx - 1);
@@ -46,31 +52,32 @@ void quicksort(int *arr, int left, int right)
 void main()
 {
 
-    clock_t start, end;
-    double cpu_time_used;
+    // Inicializa o array
+    int* array = (int*)malloc(sizeof(int) * ARR_SIZE);
+    // Pega o tamanho inicial do array
+    int array_size = ARR_SIZE;
 
+    // Leitur do array do arquivo
+    FILE *f = fopen("array65k", "rb");
+    fread(array, sizeof(int), ARR_SIZE, f);
+    fclose(f);
 
-    int* array = (int*)malloc(sizeof(int) * 33554432);
-    int array_size = 33554432;
-    for(int i=0;i<array_size;i++)
-        array[i]=rand()%100000; 
-
-    start = clock();
-    #pragma omp parallel num_threads(8)
+    // inicio do programa paralelo 
+    #pragma omp parallel num_threads(2)
     {
+        double start = omp_get_wtime();
+
+        // indica q esse codigo sera rodado por apenas uma thread
         #pragma omp single nowait
         {
-            quicksort(array, 0, 33554431);
+            quicksort(array, 0, ARR_SIZE);
         }
+
+    // Calculo de tempo de execucao
+    double end = omp_get_wtime();
+    printf("The elapsed time is %lf seconds", end-start);
     }
-    end = clock();
 
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-
-
-
-    printf("The elapsed time is %lf seconds", cpu_time_used);
 
     return;
 }
